9cba450c140ceab60f7601a784fc80aa074e8923
ksedlar <ksedlar@dhcp-18-111-56-140.dyn.mit.edu>
2017-05-03 16:55:02 -0400

    more test cases

--- project.scm
+++ project.scm
@@ -19,6 +19,18 @@
    (matrix (vector (vector 50 8 42) (vector 19 0 20))))
 ; Should produce (matrix (vector (vector 49 12 49) (vector 25 15 11)))
 
+(+ (matrix (vector (vector 1 2))) (matrix (vector (vector 3 4))))
+; Should produce (matrix (vector 4 6))
+
+(+ (matrix (vector (vector 1 2) (vector 3 4))) (matrix (vector (vector 1 2))))
+; Should produce some sort of error with matrices not being the same dimension
+
 (- (matrix (vector (vector 1 2 3) (vector 4 5 6) (vector 7 8 9)))
    (matrix (vector (vector 9 8 7) (vector -1 0 6) (vector 3 2 1))))
-; Should produce (matrix (vector (vector ) (vector ) (vector )))
+; Should produce (matrix (vector (vector -8 -6 -4) (vector 5 5 0) (vector 4 6 8)))
+
+(- (matrix (vector (vector 1 1) (vector 1 1))) (matrix (vector (vector 1 1) (vector 1 1))))
+; Should produce (matrix (vector (vector 0 0) (vector 0 0)))
+
+(- (matrix (vector 1 2) (vector 3 4)) (matrix (vector (vector 1 2))))
+; Should produce some sort of error with matrices not being the same dimension
